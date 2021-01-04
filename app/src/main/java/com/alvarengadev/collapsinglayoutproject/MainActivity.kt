3c6296515b30aa6b9d5d25fb916f206a8ff16906
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2021-01-04 10:47:39 -0300

    chore: create project

---
+++ app/src/main/java/com/alvarengadev/collapsinglayoutproject/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.alvarengadev.collapsinglayoutproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
